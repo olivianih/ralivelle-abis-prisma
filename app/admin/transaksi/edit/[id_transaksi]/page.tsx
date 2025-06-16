bd6f18715ea4b68307ee47a8cde1bc0845119be2
olivianih <oliviattan@gmail.com>
2025-06-16 11:46:34 +0700

    nonaktif edit transaksi

--- app/admin/transaksi/edit/[id_transaksi]/page.tsx
+++ app/admin/transaksi/edit/[id_transaksi]/page.tsx
@@ -1,26 +1,26 @@
-import EditTransactionForm from "@/app/components/EditTransactionForm";
-import { fetchTransaksi } from "@/app/lib/data";
+// import EditTransactionForm from "@/app/components/EditTransactionForm";
+// import { fetchTransaksi } from "@/app/lib/data";
 
-interface Props {
-  params: { id_transaksi: string };
-}
+// interface Props {
+//   params: { id_transaksi: string };
+// }
 
-export default async function EditTransaksiPage({ params }: Props) {
-  const transaksi = await fetchTransaksi();
-  const item = transaksi.find(t => t.id_transaksi === params.id_transaksi);
+// export default async function EditTransaksiPage({ params }: Props) {
+//   const transaksi = await fetchTransaksi();
+//   const item = transaksi.find(t => t.id_transaksi === params.id_transaksi);
 
-if (!item) {
-  return <div>Transaksi tidak ditemukan</div>;
-}
+// if (!item) {
+//   return <div>Transaksi tidak ditemukan</div>;
+// }
 
-const safeItem = {
-  id_transaksi: item.id_transaksi,
-  id_produk: item.id_produk ?? "",
-  nama_pelanggan: item.nama_pelanggan ?? "",
-  tanggal_transaksi: item.tanggal_transaksi ?? "",
-  total_harga: item.total_harga ?? "",
-};
+// const safeItem = {
+//   id_transaksi: item.id_transaksi,
+//   id_produk: item.id_produk ?? "",
+//   nama_pelanggan: item.nama_pelanggan ?? "",
+//   tanggal_transaksi: item.tanggal_transaksi ?? "",
+//   total_harga: item.total_harga ?? "",
+// };
 
-return <EditTransactionForm transaksi={safeItem} />;
+// return <EditTransactionForm transaksi={safeItem} />;
 
-}
+// }
